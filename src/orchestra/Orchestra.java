975c559c6868e8cc1147cad33d18264cd644feaf
Richard Matsumoto <richard.matsumoto@gmail.com>
2022-06-04 21:43:55 -0400

    add Section

--- src/orchestra/Orchestra.java
+++ src/orchestra/Orchestra.java
@@ -1,8 +1,20 @@
 package orchestra;
 
+import orchestra.section.Section;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Orchestra {
+    private List<Section> sections;
+
+    private Orchestra() {
+        this.sections = new ArrayList<Section>();
+    }
+
     private void print() {
         System.out.println("=== Orchestra Members ===");
+        this.sections.forEach(section -> section.print());
     }
 
     public static void main(String[] args) {

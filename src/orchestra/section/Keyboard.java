9e63e2f76ca04f0499269c88bcf7d9d03e51ec8d
Richard Matsumoto <richard.matsumoto@gmail.com>
2022-06-05 01:07:50 -0400

    add Keyboard and Percussion

---
+++ src/orchestra/section/Keyboard.java
@@ -0,0 +1,10 @@
+package orchestra.section;
+
+public class Keyboard extends Section {
+
+    public Keyboard() {
+        super("Keyboard");
+        addInstrument("Organ");
+        addInstrument("Phiano");
+    }
+}
